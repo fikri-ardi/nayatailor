7fc156bbbdb21c49ccbf186b8e9adf32087d1222
fikri-dev <fan10062003@gmail.com>
2022-04-02 13:19:39 +0700

    fix master layout and navigation menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,23 +14,19 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="{{ asset('fonts/bootstrap-icons.css') }}">
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
-        {{-- @include('layouts.navigation') --}}
-
-        <!-- Page Heading -->
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header ?? '' }}
-            </div>
-        </header>
+        <x-navigation></x-navigation>
 
         <!-- Page Content -->
-        <main>
+        <main class="pt-20 w-10/12 sm:w-8/12 mx-auto">
             {{ $slot }}
         </main>
     </div>
