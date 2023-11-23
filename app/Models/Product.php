5de3dd9378a3a6ad77904a18f490e351449edf07
Fikri <fan10062003@gmail.com>
2023-11-23 21:10:26 +0700

    create Product model and migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'detail',
+        'price',
+        'size',
+    ];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- database/migrations/2023_11_23_120528_create_products_table.php
+++ database/migrations/2023_11_23_120528_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name');
+            $table->text('detail');
+            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->string('size');
             $table->timestamps();
         });
     }
