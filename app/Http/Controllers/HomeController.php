c8ab5e636dc111089e5fad1af4d69c5e97c1ef15
Fikri <fan10062003@gmail.com>
2023-12-11 16:59:32 +0700

    clear some useless comment in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,10 +9,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        foreach (User::first()->chats as $chat) {
-            dd($chat->pivot->body);
-        }
-
         return view('home');
     }
 }
