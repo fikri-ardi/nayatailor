2e29e9a5a2d626adaa7f842d49b374799e1305fb
Fikri <fan10062003@gmail.com>
2024-03-24 13:34:31 +0700

    fix stuck carousel

--- resources/js/Slider.js
+++ resources/js/Slider.js
@@ -13,6 +13,7 @@ export default () => ({
         let sliderContent = this.$refs.sliderContent
         let current = slider.scrollLeft
         let offset = sliderContent.getBoundingClientRect().width
+        console.log(offset)
         slider.scrollTo({
             left: strategy(current, offset),
             behavior: 'smooth'
